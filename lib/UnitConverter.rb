7d68dab7ff2130240efa91c6634c6bf9984b9ff8
Artem <artx52135@gmail.com>
2022-10-20 08:57:03 +0300

    starting files.-m

---
+++ lib/UnitConverter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "UnitConverter/version"
+
+module UnitConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/UnitConverter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UnitConverter
+  VERSION = "0.1.0"
+end
