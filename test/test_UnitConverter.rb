f87f665a7429583ebe4005b27075f4032acbf6f9
artx <35874101+artx52135@users.noreply.github.com>
2023-01-03 01:36:02 +0300

    Update tests

--- test/test_UnitConverter.rb
+++ test/test_UnitConverter.rb
@@ -3,48 +3,57 @@
 require "test_helper"
 require_relative '../lib/UnitConverter.rb'
 
-class TestUnitConverter < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::UnitConverter::VERSION
-  end
+def test_that_it_has_a_version_number
+  refute_nil ::UnitConverter::VERSION
+end
 
-  def test_it_does_something_useful
-    assert false
-  end
-  # rlb roz rp rq rg ri
-  def test_convertion_rlb
-    assert replaceImperialUnit('2 фунта')=='900г.'
-  end
-
+def test_it_does_something_useful
+  assert false
+end
+# rlb roz rp rq rg ri rflu rft
+def test_convertion_rlb
+  assert replace_imperial_units('5 pounds')=='2.25 kilograms'
+end
 def test_convertion_roz
-    assert replaceImperialUnit('30 граммов')=='1унц.'
-  end
-
+  assert replace_imperial_units('10 ounces')=='300.0 grams'
+end
 def test_convertion_rp
-    assert replaceImperialUnit('1 пинта')=='0.56мл.'
-  end
-  def test_convertion_rq
-    assert replaceImperialUnit('2 кварты')=='2л.'
-  end
-  def test_convertion_rg
-    assert replaceImperialUnit('1 галлон')=='4л.'
-  end
-  def test_convertion_ri
-    assert replaceImperialUnit('2 дюйма')=='5см.'
-  end
-  #rl rk rgr rcm
+  assert replace_imperial_units('50 inches')=='125.0 centimeters'
+end
+def test_convertion_rq
+  assert replace_imperial_units('2 quarts')=='2.0 liters'
+end
+def test_convertion_rg
+  assert replace_imperial_units('1 gallon')=='4.0 liters'
+end
+def test_convertion_ri
+  assert replace_imperial_units('50 inches')=='125.0 centimeters'
+end
+def test_convertion_rflu
+  assert replace_imperial_units('20 fl ounces')=='600.0 grams'
+end
+def test_convertion_rft
+  assert replace_imperial_units('6 feet')=='1.83 meters'
+end
 
-  def test_convertion_rl
-    assert replaceMetricUnit('1 литр')=='1кв.' and assert replaceMetriclUnit('4 литра')=='1гл.'
-  end
-  def test_convertion_rk
-    assert replaceMetricUnit('0.9 килограмма')=='2фнт.'
-  end
-  def test_convertion_rgr
-    assert replaceMetricUnit('30 граммов')=='1унц.'
-  end
-  def test_convertion_rcm
-    assert replaceMetricUnit('5 сантиметров')=='2см.'
-  end
 
+#rl rml rk rgr rcm rm
+
+def test_convertion_rl
+  assert replace_metric_units('1 liter')=='1 quarts' and replace_metric_units('4 liters')=='1.0 gallons'
+end
+def test_convertion_rk
+  assert replace_metric_units("18 kilograms")=="40.0 pounds"
+end
+def test_convertion_rgr
+  assert replace_metric_units('500 grams')=='16.67 ounces'
+end
+def test_convertion_rcm
+  assert replace_metric_units('98 centimeters')=='39.2 inches'
+end
+def test_convertion_rm
+  assert replace_metric_units('2 meters')=='6.56 feet'
+end
+def test_convertion_rml
+  assert replace_metric_units('100 milliliters')=='3.33 fl ounces'
 end
